029951e4d4eefb185e176a32ab1ff9313334d2e9
tanln <tan_ln@163.com>
2022-04-01 15:56:09 +0800

    try vite

---
+++ vite-project/README.md
@@ -0,0 +1,12 @@
+# Try Vite
+
+Vite 构建工具，，使用 ，插件丰富
+
+优势：
+- 冷服务，基于 ESModule 通过 script 标签 （webpack 需要先打包）
+```html
+<script type="module" src="/src/main.ts"></script>
+```
+- HMR 模块热更新
+- Rollup 打包，支持 Rollup 插件
+
---
+++ vite-project/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>{{ message }}</h1>
+  <div :style="style"></div>
+</template>
+
+<script setup lang="ts">
+  const message: string = 'hello world'
+
+  type Style = {
+    backgroundColor: string,
+    height: string
+  }
+
+  const style: Style = {
+    backgroundColor: 'skyblue',
+    height: '200px'
+  }
+</script>
+
+<style>
+
+</style>
